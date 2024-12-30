ef4803af548ce3f34b194347a66a613eb3a631f5
ChetanSrikant <chetansmandiga@gmail.com>
2024-12-31 01:29:29 +0530

    added display

--- frontend/src/components/DisplayHome.jsx
+++ frontend/src/components/DisplayHome.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 
 const DisplayHome = () => {
   return (
     <div>
-      <Navbar></Navbar>
-    </div>
-  )
-}
+      <Navbar />
 
-export default DisplayHome
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl ">Fearured Charts</h1>
+
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayHome;
